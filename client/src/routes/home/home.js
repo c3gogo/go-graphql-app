8eab47b8c8c480928db91f72476af671f187e410
corentin <gogo.corentin@gmail.com>
2019-09-17 22:11:43 +0200

    feat(graphql): make mutations works

--- client/src/routes/home/home.js
+++ client/src/routes/home/home.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { gql } from 'apollo-boost'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 
 
-
 function Home () {
-    const { loading, error, data } = useQuery(gql`{
-            getNotTodos {
-                name
-                description
-            }
+  const { loading, error, data } = useQuery(
+    gql`
+      query getList {
+        getNotTodos {
+          name
+          description
         }
+      }
     `)
+
+
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+
+    function onChange (event) {
+      if (event.target.name === 'name') {
+        setName(event.target.value)
+      } else {
+        setDescription(event.target.value)
+      }
+    }
 
     let [createNotTodo] = useMutation(gql`
         mutation CreateNotTodo($name: String, $description: String) {
@@ -22,10 +35,37 @@ function Home () {
         }
     `)
 
+  function save () {
+    createNotTodo({variables: {name, description}, refetchQueries: ["getList"]})
+    setName('')
+    setDescription('')
+  }
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
-    console.log(data)
-    return data.getNotTodos.map(elem => <h1 key={elem.name}>{elem.name}</h1>)   
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', marginTop: '10%'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <h3 style={{margin: '0px'}}>Add something to not do!</h3>
+        <input placeholder='Name' name='name' onChange={onChange} value={name} />
+        <input placeholder='Description' name='description' onChange={onChange} value={description}/>
+        <button onClick={save}>Save</button>
+      </div>
+      <table style={{border: '1px solid black'}}>
+        <tr>
+          <th style={{border: '1px solid black'}}>name</th>
+          <th style={{border: '1px solid black'}}>description</th>
+        </tr>
+        {
+          data.getNotTodos.map(notTodo => (
+            <tr>
+              <td style={{border: '1px solid black'}}>{notTodo.name}</td>
+              <td style={{border: '1px solid black'}}>{notTodo.description}</td>
+            </tr>
+          ))
+        }
+      </table>
+    </div>
+  )
 }
 
 
